07609acf9c23071168fc2cde872e4c98f77ec4ef
Jason Winnebeck <gillius@gillius.org>
2017-04-27 22:21:17 -0400

    ProxyClient now takes destination URI, supporting tcp or ws scheme.

--- jagnet-examples/src/main/java/org/gillius/jagnet/examples/ProxyClient.java
+++ jagnet-examples/src/main/java/org/gillius/jagnet/examples/ProxyClient.java
@@ -23,11 +23,46 @@ import org.slf4j.LoggerFactory;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 public class ProxyClient {
 	private static final Logger log = LoggerFactory.getLogger(ProxyClient.class);
 
+	private static boolean isEmpty(String s) {
+		return s == null || s.isEmpty();
+	}
+
+	private static boolean isWebsocket(URI destination) {
+		switch (destination.getScheme()) {
+			case "tcp":
+				return false;
+			case "ws":
+				return true;
+			default:
+				System.err.println("scheme must be tcp or ws in " + destination);
+				System.exit(1);
+				return false;
+		}
+	}
+
 	public static void main(String[] args) throws Exception {
+		final URI destination;
+		if (args.length > 0)
+			destination = new URI(args[0]);
+		else
+			destination = new URI("tcp://localhost:56238/");
+
+		if (isEmpty(destination.getHost())) {
+			System.err.println("Host is undefined in " + destination);
+			System.exit(1);
+		}
+		if (destination.getPort() == -1) {
+			System.err.println("Port is undefined in " + destination);
+			System.exit(1);
+		}
+		boolean websocket = isWebsocket(destination);
+
 		EventLoopGroup group = new NioEventLoopGroup();
 		try {
 			Bootstrap b = new Bootstrap();
@@ -37,12 +72,12 @@ public class ProxyClient {
 			 .handler(new ChannelInitializer<SocketChannel>() {
 				 @Override
 				 public void initChannel(SocketChannel ch) throws Exception {
-					 if (args.length > 0 && "websocket".equalsIgnoreCase(args[0])) {
+					 if (websocket) {
 						 log.info("Running in websocket mode");
 						 ch.pipeline()
 						   .addLast(new HttpClientCodec())
 						   .addLast(new HttpObjectAggregator(8192))
-						   .addLast(new WebsocketClientHandler());
+						   .addLast(new WebsocketClientHandler(destination));
 					 }
 					 ch.pipeline()
 					   .addLast(new LineBasedFrameDecoder(512, true, true))
@@ -55,13 +90,15 @@ public class ProxyClient {
 						   p.addLast(new KryoEncoder(makeKryo()));
 						   p.addLast(new LoggingHandler());
 
-						   ch.writeAndFlush(new Message("I'm a message!"));
+						   Runnable command = () -> ch.writeAndFlush(new Message("Hello it is " + new Date()));
+
+						   ch.eventLoop().scheduleAtFixedRate(command, 0, 2, TimeUnit.SECONDS);
 					   }));
 				 }
 			 });
 
 			// Start the client.
-			ChannelFuture f = b.connect("localhost", 56238).sync();
+			ChannelFuture f = b.connect(destination.getHost(), destination.getPort()).sync();
 
 			// Wait until the connection is closed.
 			f.channel().closeFuture().sync();
@@ -96,11 +133,12 @@ public class ProxyClient {
 	private static class WebsocketClientHandler extends ChannelDuplexHandler {
 		private boolean wsActive = false;
 
-		private final WebSocketClientHandshaker handshaker = WebSocketClientHandshakerFactory.newHandshaker(
-				new URI("ws://127.0.0.1:56238/websocket"),
-		    WebSocketVersion.V13, null, false, new DefaultHttpHeaders());
+		private final WebSocketClientHandshaker handshaker;
 
-		private WebsocketClientHandler() throws URISyntaxException {
+		private WebsocketClientHandler(URI destination) throws URISyntaxException {
+			handshaker = WebSocketClientHandshakerFactory.newHandshaker(
+					destination,
+			    WebSocketVersion.V13, null, false, new DefaultHttpHeaders());
 		}
 
 		@Override
