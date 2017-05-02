1d55dc8905dc3a5a79658b7b7a5eebda2c6c7b3f
Jason Winnebeck <gillius@gillius.org>
2017-05-01 22:01:44 -0400

    Refactor enum-based state to functional state

--- jagnet-proxy-server/src/main/java/org/gillius/jagnet/proxy/server/ProxyServerHandler.java
+++ jagnet-proxy-server/src/main/java/org/gillius/jagnet/proxy/server/ProxyServerHandler.java
@@ -13,15 +13,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.List;
+import java.util.function.BiConsumer;
 
 class ProxyServerHandler extends ByteToMessageDecoder {
 	private static final Logger log = LoggerFactory.getLogger(ProxyServerHandler.class);
 
-	private enum State {
-		HEADER, TAG, PROXY
-	}
-
-	private State state = State.HEADER;
+	private BiConsumer<ChannelHandlerContext, String> state = this::headerState;
 	private final ProxyMap proxyMap;
 
 	ProxyServerHandler(ProxyMap proxyMap) {
@@ -32,42 +29,39 @@ class ProxyServerHandler extends ByteToMessageDecoder {
 	protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) throws Exception {
 		int len = in.readableBytes();
 		String line = in.readCharSequence(len, CharsetUtil.UTF_8).toString();
-		switch(state) {
-			case HEADER:
-				if (ProxyConstants.INTRO_HEADER.equals(line)) {
-					log.debug("Received good header from client");
-					state = State.TAG;
-				} else {
-					log.error("Bad header {}", line);
-					ctx.close();
-				}
-				break;
+		state.accept(ctx, line);
+	}
 
-			case TAG:
-				Promise<Channel> promise = ctx.executor().newPromise();
-				log.info("New connection for tag {} from {}", line, ctx.channel());
-				proxyMap.matchChannel(line, ctx.channel(), promise);
-				writeString(ctx, ProxyConstants.WAITING_FOR_REMOTE + "\n");
-				state = State.PROXY;
-				promise.addListener(future -> {
-					if (future.isSuccess()) {
-						log.info("Starting up new proxy tunnel from {} to {}", ctx.channel(), future.getNow());
-						writeString(ctx, ProxyConstants.CONNECTED + "\n");
-						ctx.pipeline().remove(LineBasedFrameDecoder.class);
-						ctx.pipeline().remove(this);
-						ctx.pipeline().addLast(new ProxyHandler((Channel) future.getNow()));
-//						ctx.pipeline().addLast(new LoggingHandler());
-					}
-				});
-				break;
-
-			case PROXY:
-				log.warn("Discarded line since proxy listener still active after tunnel established");
-				break;
-
-			default:
-				throw new AssertionError();
+	private void headerState(ChannelHandlerContext ctx, String line) {
+		if (ProxyConstants.INTRO_HEADER.equals(line)) {
+			log.debug("Received good header from client");
+			state = this::tagState;
+		} else {
+			log.error("Bad header {}", line);
+			ctx.close();
 		}
+	}
+
+	private void tagState(ChannelHandlerContext ctx, String line) {
+		Promise<Channel> promise = ctx.executor().newPromise();
+		log.info("New connection for tag {} from {}", line, ctx.channel());
+		proxyMap.matchChannel(line, ctx.channel(), promise);
+		writeString(ctx, ProxyConstants.WAITING_FOR_REMOTE + "\n");
+		state = this::proxyState;
+		promise.addListener(future -> {
+			if (future.isSuccess()) {
+				log.info("Starting up new proxy tunnel from {} to {}", ctx.channel(), future.getNow());
+				writeString(ctx, ProxyConstants.CONNECTED + "\n");
+				ctx.pipeline().remove(LineBasedFrameDecoder.class);
+				ctx.pipeline().remove(this);
+				ctx.pipeline().addLast(new ProxyHandler((Channel) future.getNow()));
+//						ctx.pipeline().addLast(new LoggingHandler());
+			}
+		});
+	}
+
+	private void proxyState(ChannelHandlerContext ctx, String line) {
+		log.warn("Discarded line since proxy listener still active after tunnel established");
 	}
 
 	@Override
